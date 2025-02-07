5f7986b21c2c3e487d1be2a1e5053422dd61b492
Ivan Esin <ivan.esin.78@mail.ru>
2025-02-07 18:53:20 +0400

    first commit

---
+++ reactHQ/src/App.tsx
@@ -0,0 +1,11 @@
+import HeaderComponent from './components/HeaderComponent'
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-[#112135] w-100%" style={{minHeight:"100vh"}}>
+      <HeaderComponent />
+    </div>
+  )
+}
+
+export default App
